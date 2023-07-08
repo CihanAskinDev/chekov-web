9b17e9e7e87ca423c791cc94e9e7d741072f4840
Cihan <cihan@Cihans-MacBook-Pro.local>
2023-07-08 19:17:50 -0400

    commit chekov

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
+import { useState, createContext } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css';
 import Login from './app/login/page';
 import SignUp from './app/signup/page';
+import './App.css';
+
+export const AudioContext = createContext(null);
 
 function App() {
+  const [user, setUser] = useState();
   return (
-<>
+    <AudioContext.Provider value={{ user, setUser }}>
  <BrowserRouter>
  <Routes>
   <Route path="/login" element={<Login />} />
   <Route path="/signup" element={<SignUp />} />
-  <Route path="/" element={<h1>Chekov Todo</h1>} />
+  <Route path="/" element={user ? <h1>Todo</h1> : <Login} />
  </Routes>
  </BrowserRouter>
- </>
+ </AudioContext.Provider>
   );
 }
 
---
+++ src/app/login/LoginForm.jsx
@@ -0,0 +1,73 @@
+import { useContext } from "react";
+import { initializeApp } from "firebase/app";
+import {
+     getAuth, signInWithEmailAndPassword, 
+     GoogleAuthProvider, signInWithPopup 
+    } from "firebase/auth";
+import { AuthContext } from "../../App";
+import { useNavigate } from "react-router-dom";
+const firebaseConfig = {
+apiKey: "AIzaSyBlYKXJxhytlTakCYLQXHAFbEDYARhufPA",
+authDomain: "chekov-me.firebaseapp.com",
+projectId: "chekov-me",
+storageBucket: "chekov-me.appspot.com",
+messaginSenderid: "775816859375",
+appId: "1:775816859375:web:7ef40d04e9f46bb44e151a"
+};
+
+const app = initializeApp(firebaseConfig)
+const auth = getAuth(app);
+
+
+export default function LoginForm() {
+
+    const { setUser } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    const handleGoogle = () => {
+        const provider = new GoogleAuthProvider();
+        signInWithPopup(auth, provider)
+      .then(response => {
+        setUser(response.user);
+        navigate("/");
+      })
+      .catch(err => alert(err.message));
+    }
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        signInWithEmailAndPassword(auth, email, password)
+            .then(response => {
+                setUser(response.user)
+                navigate("/"); 
+
+            })
+            .catch(err => alert(err.message))
+            
+    }
+    return (
+        <section className="bg-violet-950 p-6 rounded-lg max-w-[420px] mx-auto">
+        <form onSubmit={handleLogin} className="flex flex-col items-start justify-around min-h-[30vh]">
+            <label htmlFor="email" className="flex justify-between w-full ">
+               <span>Email</span> 
+                <input type="email" name="email" className="rounded-lg border-transparent border border-violet-500 py-2 px-8 bg-zinc-50 text-zinc-700 placeholder-zinc-400
+                text-base focus:ring-2 focus:ring-violet-600 focus:border-transparent" />
+            </label>
+           
+            <label htmlFor="password"className="flex justify-between w-full ">
+                <span>Password</span>
+                <input type="password" name="password" className="rounded-lg border-transparent border border-violet-500 py-2 px-8 bg-zinc-50 text-zinc-700 placeholder-zinc-400
+                text-base focus:ring-2 focus:ring-violet-600 focus:border-transparent" />
+            </label>
+           
+            <input type="submit" value="Login" className="bg-emerald-400 text-violet-950 py-2 px-8
+             rounded-lg shadow-lg transition duration-300 hover:bg-violet-300 cursor-pointer w-full"/>
+        </form>
+        <button onClick={handleGoogle}className="bg-emerald-400 text-violet-950 py-2 px-8
+             rounded-lg shadow-lg transition duration-300 hover:bg-blue-400 cursor-pointer" > Login with Google </button>
+    </section>
+
+    )
+}
